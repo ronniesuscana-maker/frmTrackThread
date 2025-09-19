bf82b8688cf65f19900dce6f651f84a2559bc1e2
ronniesuscana-maker <ronniesuscana@gmail.com>
2025-09-19 18:46:26 +0800

    UI

--- frmTrackThread/Form1.Designer.cs
+++ frmTrackThread/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace frmTrackThread
 {
-    partial class Form1
+    partial class frmTrackThread
     {
         /// <summary>
         /// Required designer variable.
@@ -30,37 +30,41 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnRun = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(287, 96);
+            this.label1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(217, 95);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(212, 31);
+            this.label1.Size = new System.Drawing.Size(362, 55);
             this.label1.TabIndex = 0;
             this.label1.Text = "-Thread Starts-";
             // 
-            // button1
+            // btnRun
             // 
-            this.button1.Location = new System.Drawing.Point(347, 280);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnRun.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.btnRun.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRun.Location = new System.Drawing.Point(344, 294);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(94, 34);
+            this.btnRun.TabIndex = 1;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = false;
             // 
-            // Form1
+            // frmTrackThread
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::frmTrackThread.Properties.Resources._1234;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnRun);
             this.Controls.Add(this.label1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "frmTrackThread";
+            this.Text = "frmTrackThread";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -70,7 +74,7 @@
 
         private System.Windows.Forms.Label label1;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnRun;
     }
 }
 
--- frmTrackThread/Form1.cs
+++ frmTrackThread/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace frmTrackThread
 {
-    public partial class Form1 : Form
+    public partial class frmTrackThread : Form
     {
-        public Form1()
+        public frmTrackThread()
         {
             InitializeComponent();
         }
